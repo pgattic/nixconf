507c60f9257ac677d4a9217ef587a9eccab5e218
pgattic <pgattic@gmail.com>
2026-05-01 10:49:53 -0600

    Migrate Immich to zfs pool

--- modules/features/server/immich.nix
+++ modules/features/server/immich.nix
@@ -1,15 +1,23 @@
 let port = 2283; in {
   flake.nixosModules.immich = { config, ... }: let cfg = config.my.server; in {
     services.immich = {
+      inherit port;
       enable = true;
       host = "127.0.0.1";
       accelerationDevices = null;
-      port = port;
       settings = {
         newVersionCheck.enabled = false;
       };
-      # mediaLocation = "/tank/store/immich";
+      mediaLocation = "/tank/store/immich";
     };
+    services.postgresql = {
+      dataDir = "/tank/appdata/postgresql/${config.services.postgresql.package.psqlSchema}";
+    };
+    systemd.services.immich-server.after = [ "zfs-mount.service" ];
+    systemd.services.immich-server.requires = [ "zfs-mount.service" ];
+    systemd.services.postgresql.after = [ "zfs-mount.service" ];
+    systemd.services.postgresql.requires = [ "zfs-mount.service" ];
+
     services.nginx.virtualHosts."photos.${cfg.domain}" = {
       enableACME = true;
       forceSSL = true;
